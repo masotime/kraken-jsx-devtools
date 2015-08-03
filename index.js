7c263207cffd792110dc2b06c88a1a732b45a2a3
Benjamin Goh <masotime@gmail.com>
2015-08-02 20:25:27 -0700

    Removed erroneous debug logging

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ module.exports = function (options) {
 	return function (data, args, callback) {
 		try {
 			var result = babel.transform(data.toString('utf8')).code;
-			console.log('--------------------');
-			console.log(result);
-			console.log('--------------------')
 			return callback(null, result);
 		} catch (err) {
 			return callback(err);
